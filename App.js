dd8641905be55b1e87591f0dc7709bc02406dedb
AhnMir <dksalfm1010@naver.com>
2021-08-27 12:32:35 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import SignUp from './SignUp';
+import SignUpContent from './SignUpContent';
+import MyProfile from './MyProfile';
+import MyProfileEdit from './MyProfileEdit';
+import MyProfileEditKeyword from './MyProfileEditKeyword';
+import Settings from './Settings';
+import Notice from './Notice';
+import Account from './Account';
+import AccountBlocked from './AccountBlocked';
+import Help from './Help';
+import PerInfoSetting from './PerInfoSetting'
+import WithDraw from './WithDraw'
+
+export default function App() {
+  //미완성
+  //return <SignUp/>
+  //return <SignUpContent/>
+  //return <MyProfileEditKeyword/>
+  //return <Notice/>
+  //return <PerInfoSetting/>
+  //return <WithDraw/>
+  //완성
+  return <MyProfile/>
+  //return <MyProfileEdit/>
+  //return <Settings/>
+  //return <Help/>
+  //return <Account/>
+  //return <AccountBlocked/>
+}
